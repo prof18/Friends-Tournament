1f41af46ace68268c854dd16b4f5c32a60891e41
Marco Gomiero <marco.gomiero.93@gmail.com>
2019-09-01 20:30:57 +0200

    Fix overflow when opening the backdrop. Need to fix the layout

--- lib/src/views/tournament/session_carousel.dart
+++ lib/src/views/tournament/session_carousel.dart
@@ -1,94 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:friends_tournament/src/data/model/app/ui_session.dart';
-import 'package:friends_tournament/src/ui/page_transformer.dart';
 import 'package:friends_tournament/src/ui/utils.dart';
 import 'package:friends_tournament/src/views/tournament/session_item_widget.dart';
 
-class Carousel extends StatefulWidget {
+class Carousel extends StatelessWidget {
   final List<UISession> sessions;
 
   Carousel({this.sessions});
 
   @override
-  _CarouselState createState() => _CarouselState();
-}
-
-class _CarouselState extends State<Carousel> {
-  double currentPage;
-
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      currentPage = widget.sessions.length - 1.0;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: hexToColor("#EEEEEE"),
-      child: Column(
-        children: <Widget>[
-          Expanded(child: renderBody()),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: renderBottomBar(),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget renderBody() {
-    PageController controller =
-        PageController(initialPage: widget.sessions.length - 1);
-    controller.addListener(
-      () {
-        setState(
-          () {
-            currentPage = controller.page;
-          },
-        );
-      },
-    );
-
-    return Container(
-      child: PageTransformer(
-        pageViewBuilder: (context, visibilityResolver) {
-          return PageView.builder(
-            controller: PageController(viewportFraction: 0.85),
-            itemCount: widget.sessions.length,
-            itemBuilder: (context, index) {
-              final item = widget.sessions[index];
-              final pageVisibility =
-                  visibilityResolver.resolvePageVisibility(index);
-              return SessionItemWidget(
-                session: item,
-                pageVisibility: pageVisibility,
-              );
-            },
-          );
+    return Scaffold(
+      backgroundColor: hexToColor("#eeeeee"),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        // TODO: localize me
+        label: Text("End this match"),
+        onPressed: () {
+          // TODO
         },
       ),
+      body: renderBody(context),
     );
   }
 
-  Widget renderBottomBar() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(bottom: 10.0),
-          child: FloatingActionButton.extended(
-            // TODO: localize me
-            label: Text("End this match"),
-            onPressed: () {
-              // TODO
-            },
-          ),
-        )
-      ],
+  Widget renderBody(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.7,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: sessions.length,
+          itemBuilder: (context, index) {
+            return Container(
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: SessionItemWidget(
+                session: sessions[index],
+              ),
+            );
+          }),
     );
   }
 }
--- lib/src/views/tournament/session_item_widget.dart
+++ lib/src/views/tournament/session_item_widget.dart
@@ -1,57 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:friends_tournament/src/data/model/app/ui_player.dart';
 import 'package:friends_tournament/src/data/model/app/ui_session.dart';
-import 'package:friends_tournament/src/ui/page_transformer.dart';
 import 'package:friends_tournament/src/views/tournament/session_player_tile.dart';
 
 class SessionItemWidget extends StatelessWidget {
   SessionItemWidget({
     @required this.session,
-    @required this.pageVisibility,
   });
 
   final UISession session;
-  final PageVisibility pageVisibility;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: 22.0,
-        horizontal: 8.0,
-      ),
+      padding: EdgeInsets.all(16.0),
       child: Material(
         borderRadius: BorderRadius.circular(10.0),
         elevation: 8.0,
-        child: Container(
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    session.name,
-                    style: TextStyle(
-                      fontSize: 24,
-                    ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  session.name,
+                  style: TextStyle(
+                    fontSize: 24,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemBuilder: (BuildContext context, int index) {
-                      UIPlayer player = session.sessionPlayers[index];
-                      return SessionPlayerTile(
-                        playerName: player.name,
-                        score: player.score,
-                      );
-                    },
-                    itemCount: session.sessionPlayers.length,
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  physics: ScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    UIPlayer player = session.sessionPlayers[index];
+                    return SessionPlayerTile(
+                      playerName: player.name,
+                      score: player.score,
+                    );
+                  },
+                  itemCount: session.sessionPlayers.length,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
